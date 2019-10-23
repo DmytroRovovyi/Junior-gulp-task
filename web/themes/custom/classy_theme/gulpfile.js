e1b6ddf62e3deb809d4538a925172d519287adc6
Dmytro.R <06hannibal@gmail.com>
2019-10-24 00:50:27 +0300

    compile files with gulp

---
+++ web/themes/custom/classy_theme/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+
+});
+gulp.task('sass', done => {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            browsers: ['IE 9'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css'));
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css'));
+    done();
+});
